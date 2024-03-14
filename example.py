dda832102f1d724ddc11576979643c7485a07102
bismuthadams1 <bismuthadams@gmail.com>
2024-03-14 15:03:16 +0000

    added check point weights and utility functions

--- example.py
+++
@@ -1,5 +0,0 @@
-from naglmbis.models import load_charge_model
-# load two pre-trained charge models
-charge_model = load_charge_model(charge_model="nagl-v1-mbis")
-# load a model trained to scf dipole and mbis charges
-charge_model_2 = load_charge_model(charge_model="nagl-v1-mbis-dipole")
