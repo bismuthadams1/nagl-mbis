5df44ea1ed691ee961cbe2d349e423da950f1e4b
bismuthadams1 <bismuthadams@gmail.com>
2024-03-14 16:16:40 +0000

    latest functions

--- utility_functions.py
+++ utility_functions.py
@@ -2,21 +2,52 @@ from naglmbis.models.base_model import MBISGraphModel
 import torch
 from openff.toolkit.topology import Molecule
 import rdkit
+from rdkit import Chem
+from pyCheckmol import CheckMol
 
-def get_latent_embedding(smiles: str, charge_model: MBISGraphModel) -> tuple[torch.Tensor,rdkit.Chem]:
-    """Returns the latent embeddings given a smiles string
+def get_latent_embedding(smiles: str,
+                         charge_model: MBISGraphModel) -> tuple[torch.Tensor,rdkit.Chem]:
+    """Returns the latent embeddings given a smiles string.
+
+    Parameters
+    ----------
+    smiles: str
+        Smiles string in which to grab the unique latent embedding vector
+    charge_model: MBISGraphModel
+        charge model to get the atom features for the latent embeddings
+    
+    Returns
+    -------
+    tensor: torch.Tensor
+        Tensor of shape 128 containing the atom embeddings
+    openff_mol: Molecule
+        RDKit molecule of inputted SMILES
     """
-    test_mol  = Molecule.from_smiles(smiles, allow_undefined_stereo=True)
-    test_molrd = test_mol.to_rdkit()
-    dgl_mol_2 = charge_model.return_dgl_molecule(test_molrd)
+    openff_mol  = Molecule.from_smiles(smiles, allow_undefined_stereo=True)
+    rdkit_mol = openff_mol.to_rdkit()
+    dgl_mol_2 = charge_model.return_dgl_molecule(rdkit_mol)
     charge_model.forward(dgl_mol_2)
     #this gives us our latent vector
-    return dgl_mol_2.graph.ndata['h'], test_molrd
+    return dgl_mol_2.graph.ndata['h'], rdkit_mol
 
-def total_latent_embeddings(smiles_list: list[str], charge_model: MBISGraphModel) -> torch.Tensor:
-    """
+def total_latent_embeddings(smiles_list: list[str],
+                            charge_model: MBISGraphModel, 
+                            tolerence: int = 50) -> tuple[list[torch.tensor, dict[str,torch.tensor]], dict[int,str],Chem.Mol,torch.Tensor]:
+    """Use this on a group of SMILES to get the total latent embeddings.
     
-    
+    Parameters
+    ----------
+    smiles_list: list[str]
+        list of molecules to obtain a total latent embedding tensor
+    charge_model: MBISGraphModel
+        charge model to get the atom features for the latent embeddings
+    tolerance: int
+        tolerance in which to apply to the grouping procedure of the atoms
+
+    Returns
+    -------
+    tensors: list[]
+        
     """
     atom_index = 0
     tensors = []
@@ -25,9 +56,9 @@ def total_latent_embeddings(smiles_list: list[str], charge_model: MBISGraphModel
     rdkit_mols = []
 
     for smiles in smiles_list:
-        latent_embedding, test_molrd = get_latent_embedding(smiles)
+        latent_embedding, rdkit_mol = get_latent_embedding(smiles, charge_model)
         tensors.append(latent_embedding)
-        for i, atom in enumerate(test_molrd.GetAtoms()):
+        for i, atom in enumerate(rdkit_mol.GetAtoms()):
             atom_type = atom.GetSymbol()  # Get the atom type (C, N, O, etc.)
             embedding = latent_embedding[i].unsqueeze(1)  # Get the embedding for the atom
 
@@ -40,7 +71,7 @@ def total_latent_embeddings(smiles_list: list[str], charge_model: MBISGraphModel
                 is_unique = True
                 for j, unique_embedding in enumerate(unique_embeddings[atom_type]):
                     #atol controls the distance
-                    if torch.all(torch.isclose(embedding, unique_embedding, atol=50)):
+                    if torch.all(torch.isclose(embedding, unique_embedding, atol=tolerence)):
                         atom_labels[atom_index] = f"{atom_type}{j+1}"
                         is_unique = False
                         break
@@ -53,6 +84,24 @@ def total_latent_embeddings(smiles_list: list[str], charge_model: MBISGraphModel
             atom.SetProp("atomLabel", atom_labels[atom_index])
             atom_index += 1 
 
-        rdkit_mols.append(test_molrd)    
+        rdkit_mols.append(rdkit_mol)    
 
     total_embedings = torch.cat(tensors)
+
+    return tensors, unique_embeddings, atom_labels, rdkit_mols, total_embedings
+
+def find_functional_groups(smiles: str) -> dict[str,tuple[tuple[int]]]:
+    """Based on ths smiles and the data in the pyCheckmol csv, produce a list of functional groups
+    and a tuple of atom locations of these groups. 
+
+    Parameters
+    ----------
+    smiles: str
+        smiles of inputed species
+    Returns
+    --------
+    dictionary: list[str]
+        list of functional groups
+    index 
+    
+    """
