bcf322a30af121dbb44bc0d7e51c4ed06baca9bf
bismuthadams1 <bismuthadams@gmail.com>
2024-08-30 15:27:19 +0000

    pushing split

--- scripts/dataset/split_by_deepchem.py
+++ scripts/dataset/split_by_deepchem.py
@@ -1,30 +1,32 @@
 # try spliting the entire collection of data using deepchem spliters
-import h5py
 import deepchem as dc
 import numpy as np
+from qcportal import PortalClient
+
+client = PortalClient("api.qcarchive.molssi.org")
+data_set = client.get_dataset(dataset_type='singlepoint',dataset_name='MLPepper RECAP Optimized Fragments v1.0')
+finished_records = [record for record in data_set.iterate_records(status='complete')]
 
 dataset_keys = []
 smiles_ids = []
-training_set = h5py.File("TrainingSet-v1.hdf5", "r")
-for key, group in training_set.items():
-    smiles_ids.append(group["smiles"].asstr()[0])
+
+for _, _, singlepoint in finished_records:
+    if 'ddx' in singlepoint.specification.keywords:
+        continue
+    smiles = singlepoint.molecule.identifiers.canonical_isomeric_explicit_hydrogen_mapped_smiles
+    print(smiles)
+    smiles_ids.append(smiles)
     # use the key to quickly split the datasets later
-    dataset_keys.append(key)
-training_set.close()
-
-# val_set = h5py.File('ValSet-v1.hdf5', 'r')
-# for key, group in val_set.items():
-#     smiles_ids.append(group['smiles'].asstr()[0])
-#     dataset_keys.append(key)
-
-# val_set.close()
-
+    dataset_keys.append(singlepoint.id)
 
 print(f"The total number of unique molecules {len(smiles_ids)}")
 print("Running MaxMin Splitter ...")
-
+print('smiles id list')
+print(smiles_ids)
+print('smiles set')
+print(set(smiles_ids))
 xs = np.array(dataset_keys)
-
+print(xs)
 total_dataset = dc.data.DiskDataset.from_numpy(X=xs, ids=smiles_ids)
 
 max_min_split = dc.splits.MaxMinSplitter()
@@ -34,3 +36,7 @@ train, validation, test = max_min_split.train_valid_test_split(
     valid_dir="maxmin-valid",
     test_dir="maxmin-test",
 )
+
+# train.to_csv('train_smiles.csv')
+# validation.to_csv('validation_smiles.csv')
+# test.to_csv('test_smiles.csv')
